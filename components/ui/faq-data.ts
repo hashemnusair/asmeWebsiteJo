39b1c62e796068c8306744be932ccb4f50aaee0b
Drugs_for_Hugs <49594025+Drugs-for-Hugs@users.noreply.github.com>
2025-12-07 19:05:32 +0300

    Update faq-data.ts

--- components/ui/faq-data.ts
+++ components/ui/faq-data.ts
@@ -19,12 +19,11 @@ export const faqs: Record<FAQSection, FAQItem[]> = {
     },
     {
       question: "Why should I become a member?",
-      answer: "Becoming an ASME member gives you access to valuable resources at a low cost. If you are a first-year (freshman) student, your first year of national membership is free. For other students, the annual national membership fee is about US $25. 
-        With membership you unlock benefits like discounted or free online courses, access to a library of technical articles, journals, and e-books, and savings on books and conference fees.You also become eligible to join student competitions, attend conferences, and take part in career-development webinars, all of which can strengthen your résumé and help you explore different branches of engineering.",
+      answer: "Becoming an ASME member gives you access to valuable resources at a low cost. If you are a first-year (freshman) student, your first year of national membership is free. For other students, the annual national membership fee is about US $25.\nWith membership you unlock benefits like discounted or free online courses, access to a library of technical articles, journals, and e-books, and savings on books and conference fees.You also become eligible to join student competitions, attend conferences, and take part in career-development webinars, all of which can strengthen your résumé and help you explore different branches of engineering.",
     },
     {
       question: "How do I become a member?",
-      answer: "Answer to be added soon.",
+      answer: "You can become an ASME member by registering through the official ASME Student Membership page at:\nhttps://www.asme.org/membership/student-membership\nJust create an account using your university email and fill in your basic information. First-year students receive their first year of national membership for free, while others pay a low annual fee. Once you complete the registration, you immediately gain access to all ASME student benefits.",
     },
     {
       question: "How does becoming a member benefit me?",
