7b0fc9771ce91599f30e1268bcecfbd2eed7303b
Drugs_for_Hugs <49594025+Drugs-for-Hugs@users.noreply.github.com>
2025-12-07 22:08:58 +0300

    Update faq-data.ts

--- components/ui/faq-data.ts
+++ components/ui/faq-data.ts
@@ -23,7 +23,7 @@ export const faqs: Record<FAQSection, FAQItem[]> = {
     },
     {
       question: "How do I become a member?",
-      answer: "You can become an ASME member by registering through the official ASME Student Membership page at:\n <a href='https://www.asme.org/membership/student-membership' target="_blank">ASME Student Membership Page</a>.\nJust create an account using your personal email and fill in your basic information. First-year students receive their first year of national membership for free, while others pay a low annual fee. Once you complete the registration, you immediately gain access to all ASME student benefits. If you need any help during the process, you can reach us by searching asme_uj on LinkedIn or Instagram.",
+      answer: "You can become an ASME member by registering through the official ASME Student Membership page at:\n <"<a href=\"https://www.asme.org/membership/student-membership\" target=\"_blank\">ASME Student Membership Page</a>.\nJust create an account using your personal email and fill in your basic information. First-year students receive their first year of national membership for free, while others pay a low annual fee. Once you complete the registration, you immediately gain access to all ASME student benefits. If you need any help during the process, you can reach us by searching asme_uj on LinkedIn or Instagram.",
     },
   ],
   volunteering: [
